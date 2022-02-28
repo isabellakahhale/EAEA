c57c304aa98a40b122ba1ef6c8472e30cbce9b76
Isabella Kahhale <isk17@pitt.edu>
2022-02-28 13:08:55 -0500

    fixed error in attn check question 4 and added column to idnicate whether ps had any videos where they didn't move slider bar

---
+++ MSSD_work.R
@@ -0,0 +1,46 @@
+#MSSD work with Empathic Accuracy video ratings 
+
+setwd('/Users/kellybarry/OneDrive - University of Pittsburgh/EA-EA/Data/participants/')
+
+#load in data 
+
+
+#loop code through each video rating 
+  #for each response by participant --> for each task 
+loop through for each participant folder in "responses_by_participant"
+  loop through each video (in participant folder)
+
+#find all folders in your directory
+  dir<-"/Users/kellybarry/OneDrive - University of Pittsburgh/EA-EA/Data/participants/20_ps_02212022/EA_Task/responses_by_participant"
+#get a list of all files in directory
+  file_names<-list.files(path=dir, full.names = F)  
+  file_names
+
+  
+    
+for (f in 1:nrow(file_names)) {
+  for (v in 1:nrow(videolist)){
+     <- paste ("/Users/kellybarry/OneDrive - University of Pittsburgh/EA-EA/Data/participants/20_ps_02212022/EA_Task/responses_by_participant", identifier, "/", videolist$Vid_List[v], ".csv", sep = "")
+  }
+}
+
+#subset video variables 
+
+
+library(psych)
+?psych::mssd
+
+psych::mssd(x,group=NULL, lag = 1,na.rm=TRUE)
+
+?autoR
+
+
+
+
+
+
+
+
+
+
+
